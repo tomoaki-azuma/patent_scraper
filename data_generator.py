a4b2f99ca9cfe353b4c20c05bb9d2fbfa67aa9d3
Tomoaki Azuma <azumau@gmail.com>
2023-05-20 11:56:08 +0900

    modify data_generator

---
+++ data_generator.py
@@ -0,0 +1,45 @@
+import os
+import re
+import requests
+import csv
+import sys
+import sqlite3
+from time import sleep
+from bs4 import BeautifulSoup
+from selenium import webdriver
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support import expected_conditions as ec
+from selenium.common.exceptions import TimeoutException
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.select import Select
+from selenium.webdriver.chrome import service as fs
+import pickle
+from selenium.webdriver.chrome.options import Options # オプションを使うために必要
+
+all_results = []
+
+conn = sqlite3.connect('inpass.db')
+c = conn.cursor()
+c.execute('select app_num, reg_num, patentee, patentee_address from inpass_result order by reg_num')
+inpass_results = c.fetchall()
+
+for result in inpass_results:
+  reg_num = result[1]
+
+  result_list = list(result)
+
+  c.execute('select remark from remarks where reg_num = ?', (reg_num,))
+  remarks = c.fetchall()
+
+  for remark in remarks:
+    result_list.append(remark[0])
+  
+  all_results.append(result_list)
+
+with open('scraping_results.csv', 'w', newline='') as f:
+  writer = csv.writer(f, delimiter='\t')
+  writer.writerows(all_results)
+
+conn.close()
